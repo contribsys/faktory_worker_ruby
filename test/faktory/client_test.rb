286cb13dfa31e6529fa3fb12399f9ba4583ed177
Bryan Rite <bryan@bryanrite.com>
2017-12-22 15:52:37 +0100

    Specs covering current initialization and failure.

---
+++ test/faktory/client_test.rb
@@ -0,0 +1,24 @@
+require 'helper'
+
+class ClientFaktory < Minitest::Test
+  def test_client_initialized_with_default_url
+    client = Faktory::Client.new
+    assert_equal URI("tcp://localhost:7419"), client.instance_variable_get(:@location)
+  end
+
+  def test_client_initialized_with_env
+    ENV["FAKTORY_PROVIDER"] = "FAKTORY_URL"
+    ENV["FAKTORY_URL"] = "tcp://127.0.0.1:7419"
+
+    client = Faktory::Client.new
+    assert_equal URI("tcp://127.0.0.1:7419"), client.instance_variable_get(:@location)
+  end
+
+  def test_client_initialized_with_specific_url
+    ENV["FAKTORY_PROVIDER"] = "FAKTORY_URL"
+    ENV["FAKTORY_URL"] = "tcp://127.0.0.1:7419"
+
+    client = Faktory::Client.new(url: 'tcp://lvh.me:7419')
+    assert_equal URI("tcp://lvh.me:7419"), client.instance_variable_get(:@location)
+  end
+end
