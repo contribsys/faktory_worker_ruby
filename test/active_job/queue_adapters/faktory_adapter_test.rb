1684a1d8d3ae48287805eec8fd58a54ad797ffa9
Jeremy Green <jeremy@octolabs.com>
2018-03-19 23:56:01 -0500

    Add a basic test for the AJ adapter

---
+++ test/active_job/queue_adapters/faktory_adapter_test.rb
@@ -0,0 +1,54 @@
+require 'helper'
+
+class FaktoryAdapterTest < LiveTest
+  describe 'ActiveJob adapter' do
+
+    class TestJob < ActiveJob::Base
+      self.queue_adapter = :faktory
+      cattr_accessor :count
+      @count = 0
+      def perform(*args)
+        args.each do |value|
+          self.class.count += value
+        end
+      end
+    end
+
+    before do
+      require 'faktory/testing'
+      Faktory::Testing.fake!
+      ActiveJob::QueueAdapters::FaktoryAdapter::JobWrapper.jobs.clear
+      TestJob.count = 0
+    end
+
+    after do
+      Faktory::Queues.clear_all
+      Faktory::Testing.disable!
+    end
+
+    it 'queues a job in the JobWrapper queue' do
+      assert_equal 0, ActiveJob::QueueAdapters::FaktoryAdapter::JobWrapper.jobs.size
+      TestJob.perform_later(42)
+      assert_equal 1, ActiveJob::QueueAdapters::FaktoryAdapter::JobWrapper.jobs.size
+    end
+
+    it 'can pass along a priority' do
+      TestJob.set(priority: 9).perform_later(123)
+      job = ActiveJob::QueueAdapters::FaktoryAdapter::JobWrapper.jobs.last
+      assert_equal 9, job["priority"]
+    end
+
+    it 'can perform a job' do
+      TestJob.perform_later(42)
+      ActiveJob::QueueAdapters::FaktoryAdapter::JobWrapper.drain
+      assert_equal 42, TestJob.count
+    end
+
+    it 'can perform a job with multiple arguments' do
+      TestJob.perform_later(1,2,3)
+      ActiveJob::QueueAdapters::FaktoryAdapter::JobWrapper.drain
+      assert_equal 6, TestJob.count
+    end
+
+  end
+end
