75c7904f321176edb8765a844bcb26e198e3ee8e
Robert Schulze <robert@dotless.de>
2019-12-22 14:26:38 +0100

    Move perform_* methods to Job module
    To make creating polyglot jobs easier

--- test/testing_fake_test.rb
+++ test/testing_fake_test.rb
@@ -318,4 +318,37 @@ class TestingFakeTest < LiveTest
       assert_equal 1, jobs.size
     end
   end
+
+  describe 'polyglot testing' do
+    before do
+      require 'faktory/testing'
+      Faktory::Testing.fake!
+    end
+
+    after do
+      Faktory::Testing.disable!
+      Faktory::Queues.clear_all
+    end
+
+    it 'perform_async' do
+      Faktory::Job.perform_async('someFunc', ['some', 'args'], queue: 'some_q')
+      assert_equal 1, Faktory::Queues['some_q'].size
+
+      job = Faktory::Queues['some_q'].first
+      assert_equal 'someFunc', job['jobtype']
+      assert_equal 'some_q', job['queue']
+      assert_equal ['some', 'args'], job['args']
+    end
+
+    it 'perform_in' do
+      Faktory::Job.perform_in(10, 'someFunc', ['some', 'args'], queue: 'some_q')
+      assert_equal 1, Faktory::Queues['some_q'].size
+
+      job = Faktory::Queues['some_q'].first
+      assert_equal 'someFunc', job['jobtype']
+      assert_equal 'some_q', job['queue']
+      assert_equal ['some', 'args'], job['args']
+      assert_in_delta Time.now.to_f, Time.parse(job['at']).to_f, 10.1
+    end
+  end
 end
