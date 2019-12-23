d9978abe908c05962b03ae544d24c77453d015fd
Robert Schulze <robert@dotless.de>
2019-12-23 12:52:20 +0100

    Add another test for constantized Factory::Job:Setter instance

--- test/polyglot_test.rb
+++ test/polyglot_test.rb
@@ -12,6 +12,8 @@ class PolyglotTest < LiveTest
       Faktory::Queues.clear_all
     end
 
+    SomePolyglotJob = Faktory::Job.set(queue: 'some_q', jobtype: 'someFunc')
+
     it 'perform_async' do
       Faktory::Job.set(queue: 'some_q', jobtype: 'someFunc').perform_async('some', 'args')
       assert_equal 1, Faktory::Queues['some_q'].size
@@ -30,6 +32,19 @@ class PolyglotTest < LiveTest
       assert_equal 'some_q', job['queue']
       assert_equal ['some', 'args'], job['args']
       assert_in_delta Time.now.to_f, Time.parse(job['at']).to_f, 10.1
+    end
+
+    it 'perform_async via constant' do
+      SomePolyglotJob.perform_async('some', 'args')
+      SomePolyglotJob.perform_async('some', 'args')
+
+      assert_equal 2, Faktory::Queues['some_q'].size
+
+      job = Faktory::Queues['some_q'].last
+      assert_equal 'someFunc', job['jobtype']
+      assert_equal ['some', 'args'], job['args']
+
+      assert Faktory::Queues['some_q'].first["jid"] != Faktory::Queues['some_q'].last["jid"]
     end
   end
 end
