fdf75823c79c50f1e8f07544f292d8d82db1a77f
Mike Perham <mperham@gmail.com>
2023-12-05 05:22:50 -0800

    expand pool sizing since pools are lazy anyways, fixes contribsys/faktory#455

--- lib/faktory/connection.rb
+++ lib/faktory/connection.rb
@@ -6,7 +6,7 @@ module Faktory
   class Connection
     class << self
       def create(options = {})
-        size = Faktory.worker? ? (Faktory.options[:concurrency] + 2) : 5
+        size = Faktory.worker? ? (Faktory.options[:concurrency] + 5) : 20
         ConnectionPool.new(timeout: options[:pool_timeout] || 1, size: size) do
           Faktory::Client.new(**options)
         end
